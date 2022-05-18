0cdaafc9b9edb3db98245bf0a914ca670fb0280e
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-05-19 02:07:07 +0600

    post-method

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.74crw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    const tasksCollection = client.db("todo").collection("tasks");
+    await client.connect();
+    try {
+        app.post('/tasks', async (req, res) => {
+            const query = req.body
+            const result = await tasksCollection.insertOne(query)
+            res.send(result)
+        })
+        app.get('/tasks', async (req, res) => {
+            const query = {}
+            const cursor = tasksCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
