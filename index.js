6d0d32a6d656d1d9bb47f1edd5f5656d3131e33e
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-05-19 04:04:47 +0600

    final-push

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000
 
@@ -22,9 +22,17 @@ async function run() {
             res.send(result)
         })
         app.get('/tasks', async (req, res) => {
-            const query = {}
+            const email = req.query.email
+            const query = { user: email }
             const cursor = tasksCollection.find(query)
             const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.delete('/tasks/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await tasksCollection.deleteOne(query)
             res.send(result)
         })
     }
